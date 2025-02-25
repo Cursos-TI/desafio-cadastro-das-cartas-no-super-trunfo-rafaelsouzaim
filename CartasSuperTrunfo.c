20f03c311b5daa7f807982dfff5d254a3c9611c8
Rafael Souza <1524249+rafaelsouzaim@users.noreply.github.com>
2025-02-25 20:05:57 +0000

    Lendo e exibindo código da cidade

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -9,16 +9,21 @@
 int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+    char codCidade[3];
+    int populacao, numeroPontosTuristicos;
+    float area, pib;
     
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
+    printf("Entre com o Código da Cidade: \n");
+    scanf("%s", codCidade);
     
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
-    printf("test");
+    printf("Código da cidade é: %s", codCidade);
     printf("\n");
 
     return 0;
