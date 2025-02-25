eb1b686c204567fafbb86c6dedfedfadf517acba
Rafael Souza <1524249+rafaelsouzaim@users.noreply.github.com>
2025-02-25 22:47:02 +0000

    limpando comentários iniciais

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,9 +2,6 @@
 
 // Desafio Super Trunfo - Países
 // Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
 
 int main() {
     // Variáveis para Carta 1
